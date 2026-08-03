835bc1111a88f180de9dfcf79e534d165bad8909
Ravi Kamble <ravikamble87@gmail.com>
2026-08-03 20:53:21 +0530

    JWT implementation

--- user-management-service/src/main/java/com/tradestream/usermanagementservice/controller/AuthController.java
+++ user-management-service/src/main/java/com/tradestream/usermanagementservice/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<> login(@Requestbody LoginRequest request){
+    public ResponseEntity<> login(@RequestBody LoginRequest request){
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.username(), request.password());
         )
