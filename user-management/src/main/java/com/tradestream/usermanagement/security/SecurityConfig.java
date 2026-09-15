226077e7115a6c380e9bc6a46fef7328d53aac49
Ravi Kamble <ravikamble87@gmail.com>
2026-09-15 19:59:02 +0530

    Got the swagger working

--- user-management/src/main/java/com/tradestream/usermanagement/security/SecurityConfig.java
+++ user-management/src/main/java/com/tradestream/usermanagement/security/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.tradestream.usermanagement.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated());
+        return http.build();
     }
 }
